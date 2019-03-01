dd7a1eb490819c18e6680565a5e93f727da84db0
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-03-01 09:35:27 -0700

    add rerender after adding a recipe

--- backend/server.js
+++ backend/server.js
@@ -55,22 +55,14 @@ app.get('/', function(req, res) {
 })
 
 app.get('/recipes', jwtAuth, function(req, res) {
-    console.log(req.user)
     if(!(req.user.id)) {
         res.redirect('/');
     } else {
         const userId = req.user.id;
-        console.log(userId)
         client.query('SELECT * FROM recipes WHERE user_id=$1', [userId])
         .then(result => {
-            
+            res.send(result);
         })
-        // client.query('SELECT * FROM recipes WHERE user_id=$1', [userId], (err, result) => {
-        //     if(err) {
-        //         console.error(`Error: ${err}`);
-        //     }
-        //     res.render('recipes', {recipes: result.rows});
-        // });
     }
 });
 
